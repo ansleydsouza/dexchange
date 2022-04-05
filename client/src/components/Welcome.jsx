ff46209f184519355a9d99d54206106140558a8b
Ansley <ansleydsouza@icloud.com>
2022-04-05 20:07:05 +0800

    Completed welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -24,7 +24,7 @@ const Welcome = () => {
 
     return(
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4"> 
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4"> 
                 <div className="flex flex-1 justify-start flex-col md:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Crypto <br /> across the world</h1>
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
@@ -59,7 +59,7 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
                     <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
@@ -87,15 +87,17 @@ const Welcome = () => {
 
                         <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-                        <button
+                        {false ? (
+                            <Loader />
+                        ) : (
+                            <button
                             type="button"
                             onClick={handleSubmit}
                             className="text-white w-full mt-2 border-[1px] p-2 border-[#3d47fc] rounded-full cursor-pointer hover:bg-blue-700">
                             Send Now
-                        </button>
+                            </button>
+                        )}
                     </div>
-
-                    
 
                 </div>
 
