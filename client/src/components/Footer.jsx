fe7721fd9aafb1ac655bd739256382dcea7c2835
Ansley <ansleydsouza@icloud.com>
2022-04-10 08:17:01 +0800

    Completed front end

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,16 @@
-const Footer = () => (
-    <h1>Footer</h1>
-);
+const Footer = () => {
+  return (
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+          <h1 className="text-white text-xl font-semibold w-32 cursor-pointer">
+            Unstoppable Exchange
+          </h1>
+          <h2>Contact Me</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x9fd6cDc9b456c997F5Ea0CB0053545987bEf1AC6';
